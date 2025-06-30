737a5979a1a2af5981a60ae8ae14b977d30e7ac5
Radhika <radhikatiwari1009@gmail.com>
2025-06-30 16:13:26 +0530

    Create custom_test.cpp

---
+++ templates/custom_test.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define ceach(x, a) for (const auto& x : a)
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main() {
+    fast_io;
+
+    int t; cin >> t;
+    while (t--) {
+        
+    }
+}
