a4af38bfaddc389bdef855793f70bb21e1611c12
Radhika <radhikatiwari1009@gmail.com>
2025-07-04 18:15:05 +0530

    Update custom_test.cpp

--- templates/custom_test.cpp
+++ templates/custom_test.cpp
@@ -2,15 +2,11 @@
 using namespace std;
 using ll = long long;
 #define pb push_back
-#define all(x) (x).begin(), (x).end()
-#define ceach(x, a) for (const auto& x : a)
+#define ex(x) {cout << x << endl; return 0;}
 #define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
 
 int main() {
     fast_io;
 
-    int t; cin >> t;
-    while (t--) {
-        
-    }
+    
 }
